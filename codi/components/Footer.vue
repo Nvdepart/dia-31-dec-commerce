8443598690d62df985274fb0c00a2df68a2fdda4
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-01-01 00:26:03 +0100

    delete

---
+++ codi/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card 
+        tile 
+        flat 
+        rounded="lg"
+        class="pa-md-10 pa-5 text-center" 
+        color="surface" >
+        <h2 class="text-md-h6 font-weight-bold">FreeCommerce</h2>
+        <p class="text-md-body-2 mt-5">Free eCommerce Site Built With NuxtJS ❤</p>
+        <div class="text-center">
+            <v-btn 
+                v-for="(b, i) in sm" 
+                :key="`socialMedia-${i}`" 
+                color="surface" 
+                class="mr-2" 
+                fab 
+                depressed>
+                <v-icon>{{b.icon}}</v-icon>
+            </v-btn>
+        </div>
+    </v-card>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                sm:[
+                    {
+                        icon:'mdi-facebook',
+                        link:'http://www.facebook.com',
+                    },
+                    {
+                        icon:'mdi-whatsapp',
+                        link:'http://www.wathsapp.com',
+                    },
+                    {
+                        icon:'mdi-instagram',
+                        link:'http://www.instagram.com',
+                    },
+                    {
+                        icon:'mdi-twitter',
+                        link:'http://www.twitter.com',
+                    },
+                    {
+                        icon:'mdi-youtube',
+                        link:'http://www.youtube.com',
+                    },
+                ]
+            }
+        }
+    }
+</script>
---
+++ codi/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <v-app-bar 
+            color="surface"
+            height="80" 
+            class="el" 
+            app 
+            :style="{padding : $vuetify.breakpoint.modAndUp ? '0px 100px' : '',}">
+        <v-toolbar-title 
+            class="text-md-h5 font-weight-bold pointer">
+            FreeCommerce
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn nuxt to="/" class="mr-md-2" icon>
+            <v-icon size="20">mdi-store-outline</v-icon>
+        </v-btn>
+        <v-badge
+        
+        >
+        <v-btn nuxt to="/" icon>
+            <v-icon size="20">mdi-cart-outline</v-icon>
+        </v-btn>
+        </v-badge>
+        <v-btn icon>
+            <v-icon size="20">mdi-cart-outline</v-icon>
+        </v-btn>
+        <v-divider vertical class="mx-md-5 mx-2"></v-divider>
+        <v-btn @click="toggleTheme" icon>
+            <v-icon size="20">mdi-brightness-8</v-icon>
+        </v-btn>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        toggleTheme() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        },
+  },
+}
+</script>
